c70336eb5e524cbd4cb699d630ead4ea15102adb
hao835167-dot <hao835167@gmail.com>
2025-12-01 09:15:44 +0700

    Update bai4.4.py

--- bai4.4.py
+++ bai4.4.py
@@ -1,3 +1,7 @@
+print("Tên: trần văn hào")
+print("Msv:245752021610153")
+print("#############################")
+######################################
 ds = ('Nhập danh sách: ').split()
 
 # In cả dãy vùa nhập
@@ -8,3 +12,4 @@ print(ds)
 
 for so in ds:
     print(so)
+
