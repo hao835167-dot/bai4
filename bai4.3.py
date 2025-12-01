497c58abb8887c9117f1beca4b22711e792752ee
hao835167-dot <hao835167@gmail.com>
2025-12-01 09:15:32 +0700

    Update bai4.3.py

--- bai4.3.py
+++ bai4.3.py
@@ -1,6 +1,11 @@
+print("Tên: trần văn hào")
+print("Msv:245752021610153")
+print("#############################")
+######################################
 S = input("Nhập chuỗi S: ")
 
 
 print("Các ký tự IN HOA của chuỗi:")
 for ch in S:
     print(ch.upper())
+
