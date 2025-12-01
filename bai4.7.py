4b78f1ed3476da0845a71b6af87bfbdd146b9764
hao835167-dot <hao835167@gmail.com>
2025-12-01 09:16:10 +0700

    Update bai4.7.py

--- bai4.7.py
+++ bai4.7.py
@@ -1,3 +1,8 @@
+print("Tên: trần văn hào")
+print("Msv:245752021610153")
+print("#############################")
+######################################
 s = input("Nhập chuỗi: ")
 chuoi_moi = ''.join(ch for ch in s if not ch.isdigit())
 print("Chuỗi sau khi loại bỏ chữ số:", chuoi_moi)
+
