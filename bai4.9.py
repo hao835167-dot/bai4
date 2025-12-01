42f818ea9063f8fcd8be6a724af6b269019af52e
hao835167-dot <hao835167@gmail.com>
2025-12-01 09:16:40 +0700

    Update bai4.9.py

--- bai4.9.py
+++ bai4.9.py
@@ -1,4 +1,9 @@
+print("Tên: trần văn hào")
+print("Msv:245752021610153")
+print("#############################")
+######################################
 ds = ("Nhập danh sách: ").split()
 
 # In ra toàn bộ dãy vừa nhập
 print("Danh sách vừa nhập là:",ds)
+
