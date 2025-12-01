5195ed863d3e8f21471a5ab2e42f5071bf0441c4
hao835167-dot <hao835167@gmail.com>
2025-12-01 08:55:38 +0700

    Update Bai4_2.py

--- Bai4_2.py
+++ Bai4_2.py
@@ -1,3 +1,7 @@
+print("Tên: trần văn hào")
+print("Msv:245752021610153")
+print("#############################")
+######################################
 # Bài 2: Chỉ in ra những ký tự không phải space và tab
 S = input('Nhập chuỗi: ')
 for ky_tu in S:
